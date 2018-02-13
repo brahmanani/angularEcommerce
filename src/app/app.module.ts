8b2340c760cf06574993efebab01a57275f5ed9e
brahmanani <nanibrahmaiah@gmail.com>
2018-02-13 08:44:24 +0530

    2-13-2018 updated code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Angular2FontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/navbar/navbar.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  navigation = [
+    {
+      name:"Home",
+      url:"index.html"
+    },
+    {
+      name:"Men",
+      url:"javascript:void(0)",
+      children:[
+        {
+          name:"T-Shirts",
+          url:"product-list.html"
+        },
+        {
+          name:"Casual Shirts",
+          url:"product-list.html"
+        },
+        {
+          name:"Formal Shirts",
+          url:"product-list.html"
+        },
+        {
+          name:"Jeans",
+          url:"product-list.html"
+        },
+        {
+          name:"Casual Trousers",
+          url:"product-list.html"
+        }
+      ]
+    },
+    {
+      name:"Women",
+      url:"javascript:void(0)",
+      children:[
+        {
+          name:"kurtas-kurtis-suits",
+          url:"product-list.html"
+        },
+        {
+          name:"dresses-and-jumpsuits",
+          url:"product-list.html"
+        },
+        {
+          name:"jeans-jeggings",
+          url:"product-list.html"
+        },
+        {
+          name:"Shapewear",
+          url:"product-list.html"
+        }
+      ]
+    },
+    {
+      name:"Pages",
+      url:"javascript:void(0)",
+      children:[
+        {
+          name:"product-list",
+          url:"product-list.html"
+        },
+        {
+          name:"product single",
+          url:"product-single.html"
+        },
+        {
+          name:"about",
+          url:"about.html"
+        },
+        {
+          name:"cart",
+          url:"cart.html"
+        },
+        {
+          name:"account",
+          url:"account.html"
+        },
+        {
+          name:"checkout",
+          url:"checkout.html"
+        },
+        {
+          name:"404 error",
+          url:"404-error.html"
+        }
+      ]
+    },
+    {
+      name:"Blog",
+      url:"javascript:void(0)",
+      children:[
+        {
+          name:"Blog Default",
+          url:"blog-default.html"
+        },
+        {
+          name:"Blog Single",
+          url:"blog-single.html"
+        }
+      ]
+    },
+    {
+      name:"Contact Us",
+      url:"contact-us.html"
+    }
+  ]
+
+  ngOnInit() {
+  }
+
+}
